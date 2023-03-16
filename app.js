535d152115087156aea46dbd07f9b49ff8a454cf
KingJiwon <wldnjs0401@naver.com>
2023-03-16 23:12:01 +0900

    23.03.16 db수업

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = 4000;
@@ -9,12 +11,15 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // ROUTES
 const mainRouter = require('./routes'); // index.js 생략된 것
 const userRouter = require('./routes/users');
 const boardRouter = require('./routes/board');
 const dbRouter = require('./routes/db');
+const dbBoardRouter = require('./routes/dbBoard');
+const cookieRouter = require('./routes/cookie');
 
 // MIDDLEWARE
 
@@ -22,7 +27,10 @@ app.use('/', mainRouter);
 app.use('/users', userRouter);
 app.use('/board', boardRouter);
 app.use('/db', dbRouter);
+app.use('/dbBoard', dbBoardRouter);
+app.use('/cookie', cookieRouter);
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   console.log(err.stack);
   res.status(err.statusCode);
--- routes/board.js
+++ routes/board.js
@@ -33,7 +33,7 @@ router.post('/write', (req, res) => {
 // 글 수정 모드로 이동
 router.get('/modify/:title', (req, res) => {
   const arrIndex = ARTICLE.findIndex(
-    (article) => article.title === req.params.title
+    (article) => article.title === req.params.title,
   );
   const selectedArticle = ARTICLE[arrIndex];
   res.render('board_modify', { selectedArticle });
@@ -43,7 +43,7 @@ router.get('/modify/:title', (req, res) => {
 router.post('/modify/:title', (req, res) => {
   if (req.body.title && req.body.content) {
     const arrIndex = ARTICLE.findIndex(
-      (article) => article.title === req.params.title
+      (article) => article.title === req.params.title,
     );
     ARTICLE[arrIndex].title = req.body.title;
     ARTICLE[arrIndex].content = req.body.content;
@@ -58,7 +58,7 @@ router.post('/modify/:title', (req, res) => {
 // 글 삭제
 router.delete('/delete/:title', (req, res) => {
   const arrIndex = ARTICLE.findIndex(
-    (article) => article.title === req.params.title
+    (article) => article.title === req.params.title,
   );
   ARTICLE.splice(arrIndex, 1);
   res.send('삭제 완료!');
