11716be11e1b99bbbb62572e6c60d3fce415f9c0
KingJiwon <wldnjs0401@naver.com>
2023-03-21 10:05:05 +0900

    mongo: insertMany

--- controllers/mongo.js
+++ controllers/mongo.js
@@ -7,24 +7,41 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+// insertOne
+// client.connect((err) => {
+//   const test = client.db('kdt5').collection('test');
+//   test.deleteMany({}, (deleteErr, deleteResult) => {
+//     if (deleteErr) throw deleteErr;
+//     console.log(deleteResult);
+//     test.insertOne(
+//       {
+//         name: 'pororo',
+//         age: 5,
+//       },
+//       (insertErr, insertResult) => {
+//         if (insertErr) throw insertErr;
+//         console.log(insertResult);
+//       },
+//     );
+//   });
+// });
 
+// insertMany
 client.connect((err) => {
   const test = client.db('kdt5').collection('test');
   test.deleteMany({}, (deleteErr, deleteResult) => {
     if (deleteErr) throw deleteErr;
     console.log(deleteResult);
+    test.insertMany(
+      [
+        { name: 'pororo', age: 5 },
+        { name: 'loopy', age: 6 },
+        { name: 'crong', age: 4 },
+      ],
+      (insertErr, insertResult) => {
+        if (insertErr) throw insertErr;
+        console.log(insertResult);
+      },
+    );
   });
-  test.insertOne(
-    {
-      name: 'jiwon',
-      nickname: 'kingjiwon',
-    },
-    (insertErr, insrtResult) => {
-      console.log(insrtResult);
-      const findCursor = test.find({});
-      findCursor.toArray((err, data) => {
-        console.log(data);
-      });
-    },
-  );
 });
